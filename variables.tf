9691f97f6bb76ad095b396e229c062f8b3c83f88
Danilo Silva Pacheco <danilo.pacheco@telefonica.com>
2022-10-10 18:00:20 -0300

    Adicionando projeto

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "name" {
+  type    = string
+  #default = "kubernetes-on-arm-with-oracle"
+  default = "clusterKube-pacheco"
+}
+
+/*
+Available flex shapes:
+"VM.Optimized3.Flex"  # Intel Ice Lake
+"VM.Standard3.Flex"   # Intel Ice Lake
+"VM.Standard.A1.Flex" # Ampere Altra
+"VM.Standard.E3.Flex" # AMD Rome
+"VM.Standard.E4.Flex" # AMD Milan
+*/
+
+variable "shape" {
+  type    = string
+  default = "VM.Standard.A1.Flex"
+  #default = "VM.Standard.E4.Flex"
+  #default = "VM.Standard3.Flex"
+  #default = "VM.Standard.E2.1.Micro"
+}
+
+variable "how_many_nodes" {
+  type    = number
+  default = 4
+}
+
+variable "availability_domain" {
+  type    = number
+  default = 0
+}
+
+variable "ocpus_per_node" {
+  type    = number
+  default = 1
+}
+
+variable "memory_in_gbs_per_node" {
+  type    = number
+  default = 6
+}
